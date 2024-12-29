f5d3e7d516b67f7f65571d2c292428c6d7365505
Nadilaaaa12 <nadilazaskiaauliaaulia@gmail.com>
2024-12-29 13:49:11 +0700

    Interface untuk mendefinisikan kontrak bahwa implementasinya harus menyediakan kontakRepository

---
+++ app/src/main/java/com/example/pertemuan12/di/MahasiswaContainer.kt
@@ -0,0 +1,14 @@
+package com.example.pertemuan12.di
+
+import com.example.pertemuan12.repository.MahasiswaRepository
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.example.pertemuan12.repository.NetworkKontakRepository
+import com.example.pertemuan12.service_api.MahasiswaService
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+
+interface AppContainer{
+    val kontakRepository: MahasiswaRepository
+}
+
